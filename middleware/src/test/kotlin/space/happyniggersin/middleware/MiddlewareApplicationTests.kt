77bc789ec590b00b66b12283ef5daf013e2bf244
DEv0on <bananamen02@gmail.com>
2024-08-02 19:04:04 +0200

    Initial commit

---
+++ middleware/src/test/kotlin/space/happyniggersin/middleware/MiddlewareApplicationTests.kt
@@ -0,0 +1,13 @@
+package space.happyniggersin.middleware
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MiddlewareApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
---
+++ worker/src/main/kotlin/space/happyniggersin/worker/WorkerApplication.kt
@@ -0,0 +1,11 @@
+package space.happyniggersin.worker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WorkerApplication
+
+fun main(args: Array<String>) {
+    runApplication<WorkerApplication>(*args)
+}
